d90074a5f3cabd609f97529144c0c90e95ff5d3f
Gabriel Barreto <88603031+Cyacer@users.noreply.github.com>
2023-03-18 09:46:46 -0300

    v1.1.0

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,12 @@
-document.addEventListener('keypress',(e) => {
+/*document.addEventListener('keypress',(e) => {
   if(e.key === 'Enter'){
     const btn = document.querySelector('#search');
     btn.click();
   }
-})
+})*/
 function pokemonGet(e){
-let endpoints = document.querySelector('#pokemonName').value.toLowerCase();
-const url = `https://pokeapi.co/api/v2/pokemon/${endpoints}`;
+//let endpoints = document.querySelector('#pokemonName').value.toLowerCase();
+const url = `https://pokeapi.co/api/v2/pokemon/ninetales`;
 fetch(url)
 .then((res) => res.json())
 .then((data) => {
@@ -16,10 +16,10 @@ fetch(url)
   document.querySelector('.pokebox').innerHTML = `
       <div class='pokebox-container'>
         <p>#${data.id}</p>
-        <img class='pokebox-img' src="${data.sprites.other["official-artwork"].front_default}" alt="imagem do ${endpoints} " />
+        <img class='pokebox-img' src="${data.sprites.other["official-artwork"].front_default}" alt="imagem do " />
       </div>
-      <div>
-        <p>Nome:${nome.charAt(0).toUpperCase() + nome.slice(1)}</p>
+      <div class='pokebox-espec'>
+        <p>Nome: ${nome.charAt(0).toUpperCase() + nome.slice(1)}</p>
         <p>Altura: ${altura.toLocaleString('pt-BR')} m</p>
         <p>Peso: ${peso.toLocaleString('pt-BR')} kg</p>
       </div>

5d33c3fc7d7c66ac5e38d62bef34f14cbef89056
Gabriel Barreto <88603031+Cyacer@users.noreply.github.com>
2023-03-18 10:14:50 -0300

    v1.1.1

--- src/js/script.js
+++ src/js/script.js
@@ -14,6 +14,7 @@ fetch(url)
   let peso = data.weight / 10;
   let altura = data.height / 10;
   document.querySelector('.pokebox').innerHTML = `
+    <div class='border'>
       <div class='pokebox-container'>
         <p>#${data.id}</p>
         <img class='pokebox-img' src="${data.sprites.other["official-artwork"].front_default}" alt="imagem do ${endpoints}" />
@@ -23,6 +24,7 @@ fetch(url)
         <p>Altura: ${altura.toLocaleString('pt-BR')} m</p>
         <p>Peso: ${peso.toLocaleString('pt-BR')} kg</p>
       </div>
+    </div>
   `
   e.preventDefault();
 }) .catch((error) => console.log(error))
